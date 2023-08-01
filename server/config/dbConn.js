da9f4833552d4325a62a5c2924a281fa15457844
eya <eya.khalfallah@ensi-uma.tn>
2023-08-01 13:13:49 +0100

    goood

---
+++ server/config/dbConn.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+const DATABASE_URI =`mongodb+srv://eyakhalfallah:test1234@cluster0.g1yfvoi.mongodb.net/PharmacieDB?retryWrites=true&w=majority`
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(DATABASE_URI, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true
+        });
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+export default connectDB;
--- server/server.js
+++ server/server.js
@@ -1,4 +1,22 @@
-import { express } from "express";
+import express from "express";
+const app = express();
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+
+import connectDB from "./config/dbConn.js";
+
+const PORT = process.eventNames.PORT || 3500 ;
+
+//Connect to MongoDB
+
+app.use(bodyParser.json({ limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
+app.use(cors());
+
+connectDB();
+
+mongoose.connection.once('open', () => {
+    console.log('Connected to MongoDB');
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+});
